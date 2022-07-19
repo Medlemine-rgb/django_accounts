8849a1ca064223ca7a289a5c3fecb16ef2181e40
Medlemine-rgb <amar.haddad.mohamed.lemine@gmail.com>
2022-07-19 12:06:17 +0000

    add profile model to the basic user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+'''je utilise One-to-One field pour concat : User + profile (les autre information de user ) 
+
+'''
 # Create your models here.
+
+''' username  
+    password 
+    first_name
+    laste_name
+    email '''
+
+class Profile(models.Model):
+    # concatination OneToOneField
+    user = models.OneToOneField(User,on_delete= models.CASCADE)
+    # les autre attribue de profile
+    phone_number = models.CharField(max_length=15,null=True,blank=True)
+    addres = models.CharField(max_length=50 ,null=True,blank=True)
+    #imge 
+    # age 
+    #ob
+    def __str__(self) -> str:
+        return str(self.user)
