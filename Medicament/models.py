f8e719b9f15d41f78a7a7dadeeefbc974ac55232
Medlemine-rgb <amar.haddad.mohamed.lemine@gmail.com>
2022-07-21 19:56:19 +0000

    add moment de prise , date de prise, image de medicament

--- Medicament/models.py
+++ Medicament/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 Form_phar_TYPE = (
@@ -20,8 +20,19 @@ Form_phar_TYPE = (
     ('Sachet','Sachet'),
     ('Inhalation','Inhalation'),
 )
+Moment_Prise = (
+    ('Sans importance','Sans importance'),
+    ('En dehors des repas','En dehors des repas'),
+    ('Avant le repas','Avant le repas'),
+    ('Pendant le repas','Pendant le repas'),
+    ('A pres le repas','A pres le repas'),
+    
+)
 class Medicament(models.Model):
     nom_medicament = models.CharField(max_length=50)
     pathologie = models.CharField(max_length=50)
     form_pharmaceatique = models.TextField(max_length=50,choices=Form_phar_TYPE)
     nombre_comprime = models.IntegerField()
+    moment_de_prise = models.TextField(max_length=50,choices=Moment_Prise)
+    date_de_prise = models.DateTimeField(default=datetime.now)
+    img_medical =  models.ImageField(upload_to="medical_img/",default='medical_img/default.png')
