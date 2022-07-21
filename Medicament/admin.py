be80d4fa06893d6f677e5aeb38cd36f7272cacfa
Medlemine-rgb <amar.haddad.mohamed.lemine@gmail.com>
2022-07-21 18:30:06 +0000

    add basic model Medicament

--- Medicament/admin.py
+++ Medicament/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Medicament
+
+
+admin.site.register(Medicament)
--- Medicament/models.py
+++ Medicament/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+Form_phar_TYPE = (
+    ('Comprime','Comprime'),
+    ('Gélule','Gélule'),
+    ('Sirop / solution buvable','Sirop/ solution buvable'),
+    ('Comprimé effervescent','Comprimé effervescent'),
+    ('Créme','Créme'),
+    ('Ampoule','Ampoule'),
+    ('Granules','Granules'),
+    ('Granules','Granules'),
+    ('Goutte','Goutte'),
+    ('Injection','Injection'),
+    ('Pommade','Pommade'),
+    ('Collyre','Collyre'),
+    ('Pulvérisation','Pulvérisation'),
+    ('Suppositoire','Suppositoire'),
+    ('Sachet','Sachet'),
+    ('Inhalation','Inhalation'),
+)
+class Medicament(models.Model):
+    nom_medicament = models.CharField(max_length=50)
+    pathologie = models.CharField(max_length=50)
+    form_pharmaceatique = models.TextField(max_length=50,choices=Form_phar_TYPE)
+    nombre_comprime = models.IntegerField()
